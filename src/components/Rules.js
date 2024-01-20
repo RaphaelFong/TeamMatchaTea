c42bb2c0b4af51e69bec1fa9b1c41b54f79bff5d
yz <116947122+yong-zhuo@users.noreply.github.com>
2024-01-20 18:38:08 +0800

    Added quiz
    -added quiz

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,5 +1,6 @@
 import * as shuffle from './Shuffle.js'
 import * as location from './Location.js'
+import * as quiz from './Quiz.js'
 
 
 export const allRules = [
@@ -20,24 +21,55 @@ export const allRules = [
     },
     {
       id: 4,
-      check: (pw) => pw.includes('February'),
-      message: 'The word "February" must be in your password'
+      check: (pw) => {
+        const months = ['january', 'feburary', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+        for (const month of months) {
+          if (pw.includes(month)) {
+            return true;
+          }
+        }
+      },
+      message: 'Your password must include a month of the year (in lowercases)'
     },
     {
       id: 5,
-      check: (pw) => pw.includes('20'),
-      message: 'The number "20" must be in your password'
+      check: (pw) => {
+        const currYear = new Date().getFullYear();
+
+        return pw.includes(currYear.toString());
+      },
+      message: 'Your password must include the current year'
     },
     {
       id: 6,
+      check: (pw) => {
+        for (const answers of quiz.randomQuiz.ans) {
+          if (pw.includes(answers)) {
+            return true;
+          }
+        }
+      },
+      message: `Quiz Time! Include the answer (without space or lowercase) to the following quiz in your password: ${quiz.randomQuiz.quiz}`
+    },
+    {
+      id: 7,
       check: (pw) => pw.includes(location.randomLocation.place),
       message: <div> <p>This location must be in your password (no spacing and in lowercase)</p> <img class="img" src= {location.randomLocation.link} alt="Random"></img></div>
       
     },
     {
-      id: 7,
+      id: 8,
       check: (pw) => pw.includes(shuffle.requiredWord),
       message: `Decipher and include this famous Singapore slang in your password: ${shuffle.shuffledWord}`
+    },
+    {
+      id: 9,
+      check: (pw) => {
+        if (pw.includes(`${pw.length}`)) {
+          return true;
+        }
+      },
+      message: 'Your password must include the length of your password'
     }
     
   ];
